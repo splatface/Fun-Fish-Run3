610765c7a5d2e1c12cdb6ece504bb661e124f9d6
Tiffany Shao <tiffanyshao73@gmail.com>
2025-11-05 10:54:52 -0500

    did some animation stuff; still a little bit buggy but the first stage works

--- Assets/Scripts/PlayerFishImage.cs
+++ Assets/Scripts/PlayerFishImage.cs
@@ -5,27 +5,43 @@ public class PlayerFish : MonoBehaviour
 {
 
     //IMAGE VARIABLES
-    // an array of all sprite images for evolution (in order by index)
-    public Sprite[] sprites;
-    private int _currentIndex; // current index of the sprites array
-    // current sprite icon equipped
+    public Sprite[] Sprites; // array of all sprites
     private SpriteRenderer _currentSprite;
+    public RuntimeAnimatorController[] AnimationsController; // all animations for sprites (same indexes as sprites array)
+    private Animator _currentAnimation;
+    private int _currentIndex; // current index of the sprites array
+
 
     //LOGISTICS VARIABLES
-    private int _stepValue = 3; // the exponential amount of extra exp needed to evolve
+    private static int _stepValue = 3; // the exponential amount of extra exp needed to evolve
     private int _currentExpToEvolve = 100;
     private int _exp; // how much exp the playerfish currently has
+
 
     // logic behind sprite evolution
     private void SpriteEvol()
     {
+        this._currentAnimation.runtimeAnimatorController = AnimationsController[_currentIndex];
+
+        // get feedback exp from plants code
+
+        // logic of evolution
+        if (this._exp > this._currentExpToEvolve)
+        {
+            this._currentExpToEvolve *= _stepValue;
+            _currentIndex += 1;
+            // updates the sprites and animations
+            this._currentSprite.sprite = Sprites[_currentIndex];
+            this._currentAnimation.runtimeAnimatorController = AnimationsController[_currentIndex];
+        }
     }
 
     void Start()
     {
         //sets the sprite to the first evolution upon start up
         this._currentSprite = GetComponent<SpriteRenderer>();
-        this._currentSprite.sprite = sprites[0];
+        this._currentAnimation = GetComponent<Animator>();
+        this._currentSprite.sprite = Sprites[0];
         this._currentIndex = 0;
         this._exp = 0;
     }
