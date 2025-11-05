6f582be5d79ff2d78501cee55c64455be7212200
Tiffany Shao <tiffanyshao73@gmail.com>
2025-11-05 09:29:21 -0500

    updated some of the fishplayer assets to be diff and more fun

--- Assets/Scripts/PlayerFishImage.cs
+++ Assets/Scripts/PlayerFishImage.cs
@@ -1,23 +1,33 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerFish : MonoBehaviour
 {
 
-    // sprite evolution cycle
-
+    //IMAGE VARIABLES
+    // an array of all sprite images for evolution (in order by index)
+    public Sprite[] sprites;
+    private int _currentIndex; // current index of the sprites array
     // current sprite icon equipped
     private SpriteRenderer _currentSprite;
 
-    public Sprite[] sprites;
+    //LOGISTICS VARIABLES
+    private int _stepValue = 3; // the exponential amount of extra exp needed to evolve
+    private int _currentExpToEvolve = 100;
+    private int _exp; // how much exp the playerfish currently has
+
+    // logic behind sprite evolution
+    private void SpriteEvol()
+    {
+    }
 
     void Start()
     {
+        //sets the sprite to the first evolution upon start up
         this._currentSprite = GetComponent<SpriteRenderer>();
         this._currentSprite.sprite = sprites[0];
-    }
-    public void SpriteEvol()
-    {
-
+        this._currentIndex = 0;
+        this._exp = 0;
     }
 
     // Update is called once per frame
--- Assets/Scripts/PlayerFishMovement.cs
+++ Assets/Scripts/PlayerFishMovement.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class PlayerFishMovement : MonoBehaviour
 {
-    private float _movementSpeed = 5f;
+    private float _movementSpeed = 2f; // how fast the fish moves
     private Vector3 _startPos = new Vector3(-2f, 0f, 0);
 
     void Start()
@@ -13,7 +13,7 @@ public class PlayerFishMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float verticalMovement = Input.GetAxisRaw("Vertical");
+        float verticalMovement = Input.GetAxisRaw("Vertical"); // gets the vertical (ws, up, down) input
 
         transform.position += _movementSpeed * Time.deltaTime * new Vector3(0f, verticalMovement);
     }
