be7d040a03c3c1ac4052f68575c7c451e8af8a1f
hselenal <selenaliao2008@gmail.com>
2025-11-06 12:46:15 -0500

    changed tag of fish food

--- Assets/Scripts/FishFood.cs
+++ Assets/Scripts/FishFood.cs
@@ -5,14 +5,9 @@ public class FishFood : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private int _speed;
     private int _xpToGive;
-    private Rigidbody2D rb;
-    //int randomSpeed = Random.Range(1, 10);
-    //int randomXP = Random.Range(1, 10);
-
 
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
         int randomSpeed = Random.Range(4, 10);
         int randomXP = Random.Range(3, 8);
         this._speed = randomSpeed;
